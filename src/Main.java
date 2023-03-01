4298d31ee548f96b269e67a3d0db9ed41d404282
Dmitry Smirnow <Dima_Smirnoff@mail.ru>
2023-03-01 11:41:59 +0300

    first

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+    public class Main {
+        public static void main(String[] args) {
+            System.out.println(" Введите пять чисел через пробел (Продажи с понедельника по пятницу) и нажмите ENTER");
+            Scanner scanner = new Scanner(System.in);
+            int[] sales = new int[5];
+            for (int i = 0; i < sales.length; i++)
+                sales[i] = scanner.nextInt();
+            SalesManager salesManager = new SalesManager(sales);
+            int max = salesManager.max();
+            System.out.println("Максимальная сумма продаж в день  " + max);
+
+        }
+    }
